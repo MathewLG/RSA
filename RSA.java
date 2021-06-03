2b9f0ef5e66bde7f1ef332e7cf9f75d73b16188a
MathewLG <mathewlopezg@gmail.com>
2021-06-02 20:52:57 -0500

    Corrections on public key

--- RSA.java
+++ RSA.java
@@ -4,6 +4,7 @@
 
 import java.math.BigInteger;
 import java.security.SecureRandom;
+import java.util.Random;
 
 public class RSA{
 
@@ -13,14 +14,28 @@ public class RSA{
     public BigInteger llavePrivada;
     public BigInteger llavePublica;
     private BigInteger modulo;
+    private BigInteger e;
+    private int bitlength = 1024;
+    //private Random r;
+    private BigInteger d;
+    String llave = "";
 
     //Generar los numeros primos para las llaves de tamaño N
     RSA(int N){
         BigInteger p = BigInteger.probablePrime(N/2, random);
         BigInteger q = BigInteger.probablePrime(N/2, random);
         modulo = p.multiply(q);
+        e = BigInteger.probablePrime(N / 2, random);
         BigInteger phi = (p.subtract(one)).multiply(q.subtract(one));
-        llavePublica = new BigInteger("65537");
+        while (phi.gcd(e).compareTo(BigInteger.ONE) > 0 && e.compareTo(phi) < 0)
+        {
+            e.add(BigInteger.ONE);
+        }
+        d = e.modInverse(phi);
+        llave += e;
+        llave += d;
+        //System.out.println(llave);
+        llavePublica = new BigInteger(llave);
         llavePrivada = llavePublica.modInverse(phi);
     }
 
